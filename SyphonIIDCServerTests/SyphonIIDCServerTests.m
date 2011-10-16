0c58d205658ea57430cab5eaf18bb4052bf561f5
micrem73 <michele.cremaschi@gmail.com>
2011-10-16 20:09:43 +0200

    first commit

---
+++ SyphonIIDCServerTests/SyphonIIDCServerTests.m
@@ -0,0 +1,32 @@
+//
+//  SyphonIIDCServerTests.m
+//  SyphonIIDCServerTests
+//
+//  Created by Michele Cremaschi on 16/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SyphonIIDCServerTests.h"
+
+@implementation SyphonIIDCServerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SyphonIIDCServerTests");
+}
+
+@end
